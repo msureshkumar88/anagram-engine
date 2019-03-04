54562a9280af4f368b7461e38b1e55243393c98d
suresh <suresh@cleato.com>
2019-03-04 23:45:43 +0000

    implement homepage

--- controllers/main.py
+++ controllers/main.py
@@ -6,39 +6,20 @@ import template_engine
 
 from models.anagram import Anagram
 from library.helper import Helper
-
+from library.user import UserController
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        # URL that will contain a login or logout link
-        # and also a string to represent this
-        url = ''
-        url_string = ''
-        # pull the current user from the request
-        user = users.get_current_user()
-        # aaa = Anagram(tags = ["abc","abc","abc","abc"], ena_count = len(["abc","abc","abc","abc"]))
-        # aaa.put()
-
-        #logging.info(Helper.get_word_key("asdsadssdsd"))
-        Helper.validate_string("abcAAA")
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_string = 'logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_string = 'login'
-
+        user = UserController.get_user(self)
         template_values = {
-            'url' : url,
-            'url_string' : url_string,
-            'user' : user
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user
         }
-
-        # pull the template file and ask jinja to render
-        # it with the given template values
         template = template_engine.JINJA_ENVIRONMENT.get_template('views/home.html')
         self.response.write(template.render(template_values))
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
--- library/user.py
+++ library/user.py
@@ -3,7 +3,7 @@ from google.appengine.ext import ndb
 from models.user import User
 
 
-class User:
+class UserController:
     @classmethod
     def get_user(cls, current_user):
 
@@ -13,10 +13,10 @@ class User:
         if user:
             url = users.create_logout_url(current_user.request.uri)
             url_string = 'Logout'
-            myuser_key = ndb.Key('UserModel', user.email())
+            myuser_key = ndb.Key('User', user.email())
             myuser = myuser_key.get()
             if myuser == None:
-                myuser = UserModel(id=user.email(), email=user.email())
+                myuser = User(id=user.email(), email=user.email())
                 myuser.put()
         else:
             url = users.create_login_url(current_user.request.uri)
