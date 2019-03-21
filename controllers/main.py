19e2eb4b794acf8fe7a55e88d273d18654194f78
suresh <suresh12_dmm@yahoo.com>
2019-03-21 23:06:06 +0000

    implement generate anagram

--- controllers/main.py
+++ controllers/main.py
@@ -8,6 +8,7 @@ from models.anagram import Anagram
 from library.helper import Helper
 from library.user import UserController
 
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
@@ -17,6 +18,31 @@ class MainPage(webapp2.RequestHandler):
             'url_string': user['url_string'],
             'user': user
         }
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/home.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        user = UserController.get_user(self)
+        errors = ""
+
+        word = self.request.get('word')
+        anagrams = []
+        if Helper.validate_string(word) == None:
+           errors = "Please enter valid word"
+
+        if  len(errors) == 0:
+            anagrams = Helper.anagrams(word)
+
+        logging.info(anagrams)
+        template_values = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user,
+            'anagrams': anagrams,
+            'errors':errors
+        }
+
         template = template_engine.JINJA_ENVIRONMENT.get_template('views/home.html')
         self.response.write(template.render(template_values))
 
