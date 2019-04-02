105bafbeb579a23b5f0de201a46fd955e6488b0b
suresh <suresh12_dmm@yahoo.com>
2019-04-02 11:06:21 +0100

    move anagram search to home page

--- controllers/main.py
+++ controllers/main.py
@@ -9,7 +9,7 @@ from library.helper import Helper
 from library.user import UserController
 from models.user import User
 from models.anagram import Anagram
-
+from google.appengine.ext import ndb
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -26,27 +26,24 @@ class MainPage(webapp2.RequestHandler):
     def post(self):
         self.response.headers['Content-Type'] = 'text/html'
         user = UserController.get_user(self)
-        errors = ""
-
+        errors = []
         word = self.request.get('word')
+        anagrams_request = ndb.Key('Anagram', Helper.get_word_key(user["user"].email, word))
+        anagrams_request = anagrams_request.get()
         anagrams = []
-        if Helper.validate_string(word) == None:
-           errors = "Please enter valid word"
+        if anagrams_request:
+            anagrams = anagrams_request.anagram
+        else:
+            errors.append("Anagrams not available for this search")
 
-        if  len(errors) == 0:
-            anagrams = Helper.anagrams(word)
-
-        logging.info(anagrams)
-        template_values = {
+        data = {
             'url': user["url"],
             'url_string': user['url_string'],
-            'user': user,
-            'anagrams': anagrams,
-            'errors':errors
+            'errors': errors,
+            'anagrams': anagrams
         }
-
         template = template_engine.JINJA_ENVIRONMENT.get_template('views/home.html')
-        self.response.write(template.render(template_values))
+        self.response.write(template.render(data))
 
 
 app = webapp2.WSGIApplication([
